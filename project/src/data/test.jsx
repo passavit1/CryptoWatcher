b62d8992758001f7998e3f636a6dd298014884dd
Wizat <passavit1@gmail.com>
2023-02-18 21:49:58 +0700

    Commite before Add new function

---
+++ project/src/data/test.jsx
@@ -0,0 +1,19 @@
+// const axios = require("axios");
+
+// const options = {
+//   method: "GET",
+//   url: "https://developers.coinmarketcal.com/v1/events",
+//   headers: {
+//     Accept: "application/json",
+//     "Accept-Encoding": "deflate, gzip",
+//     "x-api-key": "rQnRoIadLb1wjlZkDZnTVeUFTRgIQVP5Bj60Hpx4",
+//   },
+// };
+
+// axios(options)
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
